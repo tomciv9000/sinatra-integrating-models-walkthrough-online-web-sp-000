7ae67a67e64e081df054b09010e49cc21dbc6366
Tom White <eugene.white@gmail.com>
2019-09-21 02:09:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,9 +6,12 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
+ post '/' do
     text_from_user = params[:user_text]
-
+  
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+  
     erb :results
   end
+  
 end
